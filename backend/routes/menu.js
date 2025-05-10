261eab9dda172def3291ae43afc4bb250c3b90e8
biraj buddhacharya <birajbuddhacharya@gmail.com>
2025-05-11 01:04:42 +0545

    adminAuth: fully protected routes and frontend navigation

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const menuController = require("../controllers/menuController")
-
+const {verifyAdminToken} = require('../firebase/authMiddleware')
 const router = express.Router()
 
 router.post("/insertItem", menuController.insertItem)
 router.get('/getItems', menuController.getItems)
 router.get('/getCategories', menuController.getCategories)
-router.delete('/deleteItem/:id', menuController.deleteItem)
+router.delete('/deleteItem/:id', verifyAdminToken, menuController.deleteItem)
 
 module.exports = router;
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -8,8 +8,8 @@ router.post('/cancelOrder', verifyFirebaseToken, orderController.cancelOrder)
 router.get('/getAddress', verifyFirebaseToken, orderController.getAddress)
 router.get('/getOrders', verifyFirebaseToken, orderController.getOrders)
 router.get('/getDeliveryCost', verifyFirebaseToken, orderController.getDeliveryCost)
-router.get('/getAllOrders', orderController.getAllOrders)
-router.put('/updateOrderStatus', orderController.updateOrderStatus)
-router.delete('/deleteOrder/:id', orderController.deleteOrder)
+router.get('/getAllOrders', verifyAdminToken, orderController.getAllOrders)
+router.put('/updateOrderStatus', verifyAdminToken, orderController.updateOrderStatus)
+router.delete('/deleteOrder/:id',verifyAdminToken, orderController.deleteOrder)
 
 module.exports = router; 
